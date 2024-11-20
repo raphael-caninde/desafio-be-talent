4850a06eb7286e999cb364d91bc60d436b79a31a
raphael-oliveira-kuke <raphael.oliveira@kuke.com.br>
2024-11-20 15:42:25 -0300

    feat: add header

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import Home from './pages/Home';
 
-createRoot(document.getElementById('root')!).render(<StrictMode></StrictMode>);
+import './global.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Home />
+  </StrictMode>
+);
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+
+      <h2>Home</h2>
+    </div>
+  );
+}
