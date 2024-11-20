2943504fbe481e7ee7c567e6f508f41e7b17e380
raphael-oliveira-kuke <raphael.oliveira@kuke.com.br>
2024-11-20 19:38:15 -0300

    feat: create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,15 @@
 import styles from './Header.module.css';
+import beTalent from '../../assets/logo/be-talent.png';
 
 export default function Header() {
-  return <div className={styles.container__header}>Header</div>;
+  return (
+    <header className={styles.container__header}>
+      <div className={styles['header--content']}>
+        <img
+          src={beTalent}
+          alt='Logotipo BeTalent'
+        />
+      </div>
+    </header>
+  );
 }
