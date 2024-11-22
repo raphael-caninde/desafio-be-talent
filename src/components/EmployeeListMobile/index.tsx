5d92fb860f922e86a7856fdff5f2ffb25a21906c
raphael-oliveira-kuke <raphael.oliveira@kuke.com.br>
2024-11-21 22:10:30 -0300

    feat: create component employee list mobile

---
+++ src/components/EmployeeListMobile/index.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { EmployeeType } from '../../@types/employeeType';
+import { formatDate } from '../../utils/formatDate';
+import { formatPhone } from '../../utils/formatPhone';
+import { FaAngleDown } from 'react-icons/fa';
+import styles from './EmployeeListMobile.module.css';
+
+interface Props {
+  filteredEmployees: EmployeeType[];
+}
+
+export default function EmployeeListMobile({ filteredEmployees }: Props) {
+  const [expandedEmployee, setExpandedEmployee] = useState<number | null>(null);
+
+  const toggleDropdown = (id: number) => {
+    setExpandedEmployee(expandedEmployee === id ? null : id);
+  };
+
+  return (
+    <div className={styles['container-mobile']}>
+      <div className={styles['container-mobile-header']}>
+        <h2>Foto</h2>
+        <h2>Nome</h2>
+        <div className={styles['dot-white']} />
+      </div>
+
+      {filteredEmployees &&
+        filteredEmployees.map(employee => (
+          <div
+            key={employee.id}
+            className={`${styles['employee-item']} ${
+              expandedEmployee === employee.id ? styles['expanded'] : ''
+            }`}
+          >
+            <div
+              onClick={() => toggleDropdown(employee.id)}
+              className={styles['employee-summary']}
+            >
+              <img
+                src={employee.image}
+                alt={employee.name}
+                className={styles['employee-photo']}
+              />
+
+              <p className={styles['employee-name']}>{employee.name}</p>
+
+              <FaAngleDown
+                size={25}
+                className={styles['icon']}
+              />
+            </div>
+
+            {expandedEmployee === employee.id && (
+              <div className={styles['employee-details']}>
+                <p>
+                  <strong>Cargo:</strong> {employee.job}
+                </p>
+                <p>
+                  <strong>Data de admissão:</strong>{' '}
+                  {formatDate(employee.admission_date)}
+                </p>
+                <p>
+                  <strong>Telefone:</strong> {formatPhone(employee.phone)}
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+    </div>
+  );
+}
